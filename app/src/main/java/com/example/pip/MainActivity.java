87cfa389614aba2f421dfca288e0a8cd2eea6337
Deven79 <princedeven79@gmail.com>
2019-05-20 21:54:02 +0530

    Picture In Picture Mode

---
+++ app/src/main/java/com/example/pip/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.pip;
+
+import android.app.PictureInPictureParams;
+import android.content.res.Configuration;
+import android.graphics.Point;
+import android.os.Build;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Rational;
+import android.view.Display;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActionBar actionBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        actionBar = getSupportActionBar();
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    Display display = getWindowManager().getDefaultDisplay();
+                    Point size = new Point();
+                    display.getSize(size);
+                    int width = size.x;
+                    int height = size.y;
+
+                    Rational aspectRatio = new Rational(width, height);
+                    PictureInPictureParams.Builder mPictureInPictureParams = new PictureInPictureParams.Builder();
+                    mPictureInPictureParams.setAspectRatio(aspectRatio).build();
+                    enterPictureInPictureMode(mPictureInPictureParams.build());
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onPictureInPictureModeChanged(boolean isInPictureInPictureMode, Configuration newConfig) {
+        if (isInPictureInPictureMode){
+            actionBar.hide();
+        } else {
+            actionBar.show();
+        }
+    }
+}
